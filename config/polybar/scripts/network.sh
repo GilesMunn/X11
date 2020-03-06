b1b6bb289b80487aaabafe6a89f4d658b5e11d24
GilesMunn <giles@munn.co.uk>
2020-03-06 07:45:06 +0000

    Moved more configuration stuff over.

---
+++ config/polybar/scripts/network.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+WIFI_STATUS=$(cat /sys/class/net/wlp2s0/operstate);
+ETH_STATUS=$(cat /sys/class/net/enp1s0/operstate);
+CONNECTION=$(nmcli -t -f name connection show --active);
+if [ "$WIFI_STATUS" = "up" ];
+then
+  ICON="";
+elif [ "$ETH_STATUS" = "up" ];
+then
+  ICON="";
+else
+  ICON="";
+  CONNECTION="Disconnected"
+fi
+echo "$ICON  $CONNECTION"
+
